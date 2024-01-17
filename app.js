35314cc15cc1e73f44a316e9e16a75b850b0c3e4
Nielven Mascariñas <mascarinas022@gmail.com>
2024-01-18 02:50:15 +0800

    Add lead deletion functionality and local storage support

--- app.js
+++ app.js
@@ -1,23 +1,53 @@
 let myLeads = [];
+let oldLeads = [];
 const inputEl = document.querySelector("#input-el");
 const inputBtn = document.querySelector("#input-btn");
 const ulEL = document.querySelector("#ul-el");
+const deleteBtn = document.querySelector("#delete-btn");
 
-inputBtn.addEventListener("click", function () {
-  myLeads.push(inputEl.value);
-  console.log(myLeads);
-  inputEl.value = "";
-  renderLeads();
-});
+// Get the leads from the localStorage - PS: JSON.parse()
+// Store it in a variable, leadsFromLocalStorage
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+// 1. Check if leadsFromLocalStorage is truthy
+// 2. If so, set myLeads to its value and call renderLeads()
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  render(myLeads);
+}
 
-function renderLeads() {
+function render(leads) {
   let listItems = "";
-  for (let i = 0; i < myLeads.length; i++) {
+  for (let i = 0; i < leads.length; i++) {
     listItems += `<li>
-                     <a href = "${myLeads[i]}" target = "_blank">
-                        ${myLeads[i]}
+                     <a href = "${leads[i]}" target = "_blank">
+                        ${leads[i]}
                      </a>
                   </li>`;
   }
   ulEL.innerHTML = listItems;
 }
+
+// 2. Listen for double clicks on the delete button (google it!)
+// 3. When clicked, clear localStorage, myLeads, and the DOM
+deleteBtn.addEventListener("click", () => {
+  console.log("Clicked!");
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
+
+//Setting-up local storage
+// HINTS:
+// localStorage.setItem(key, value)
+// localStorage.getItem(key)
+// localStorage.clear()
+// PS: both key and value need to be strings
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  // Save the myLeads array to localStorage
+  // PS: remember JSON.stringify()
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
+});

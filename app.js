74d934fb0b38c5ce91102630fd7dab38bb203024
Nielven Mascariñas <mascarinas022@gmail.com>
2024-01-18 00:33:38 +0800

    Refactor app.js and index.html, add list rendering functionality

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
-function saveButton() {
-  console.log("btn click!");
+let myLeads = [];
+const inputEl = document.querySelector("#input-el");
+const inputBtn = document.querySelector("#input-btn");
+const ulEL = document.querySelector("#ul-el");
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  console.log(myLeads);
+  inputEl.value = "";
+  renderLeads();
+});
+
+function renderLeads() {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += `<li>
+                     <a href = "${myLeads[i]}" target = "_blank">
+                        ${myLeads[i]}
+                     </a>
+                  </li>`;
+  }
+  ulEL.innerHTML = listItems;
 }
